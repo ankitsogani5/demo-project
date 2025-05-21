d692fec060bc8229662b18001356b834c20089e4
ankitsogani5 <59190980+ankitsogani5@users.noreply.github.com>
2025-05-21 16:21:06 +0530

    File: src/main/java/com/zensar/ankit/demo/controller/UserController.java

--- src/main/java/com/zensar/ankit/demo/controller/UserController.java
+++ src/main/java/com/zensar/ankit/demo/controller/UserController.java
@@ -1,8 +1,7 @@
 package com.zensar.ankit.demo.controller;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,24 +15,39 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+/**
+ * Controller for handling User registration operations
+ */
 @RestController
 @RequestMapping(value = "user")
-@Api(value="user")
+@Api(value = "user", tags = {"User Management"})
 public class UserController {
-	
-	@Autowired
-	private UserService userService;
-	
-	
-	@ApiOperation(value = "register user",response = User.class)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "user Successfully registered"),
-            @ApiResponse(code = 400, message = "Bad Request, request provided is not valid"),
-            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
-    })
+
+	private final UserService userService;
+
+	/**
+	 * Constructor injection for better testability
+	 * 
+	 * @param userService the user service implementation
+	 */
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	/**
+	 * Creates a new user in the system
+	 * 
+	 * @param user the user data to be registered
+	 * @return the created user with generated ID
+	 */
+	@ApiOperation(value = "Register a new user", response = User.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "User successfully registered"),
+			@ApiResponse(code = 400, message = "Bad Request, request provided is not valid"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	})
 	@PostMapping(value = "/")
 	public User create(@Valid @RequestBody User user) {
 		return userService.save(user);
 	}
-
 }
